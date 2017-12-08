f9b1cb1b4c2801a4f02530c046ecb270c3e47e60
Martin Bergljung <gravitonian@gmail.com>
2017-12-08 11:51:15 +0000

    Updated how libs are configured, and added some libs in main

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import 'hammerjs';
+import 'chart.js';
+import 'ng2-charts';
 
 import pdfjsLib from 'pdfjs-dist';
 pdfjsLib.PDFJS.workerSrc = 'pdf.worker.js';
